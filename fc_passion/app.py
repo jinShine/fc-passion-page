d3ddcd8e80ac3041666f7e1af158731e0a05ca2e
jinnify <seungjin429@gmail.com>
2020-04-17 01:26:37 +0900

    login session 구현

--- fc_passion/app.py
+++ fc_passion/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, jsonify, request, session, redirect, url_for
+from flask import Flask, render_template, jsonify, request, session, url_for, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
+app.secret_key = b'1234wqerasdfzxcv' 
 
 client = MongoClient('localhost', 27017)
 db = client.fcpassion
@@ -30,14 +31,27 @@ def login():
                 "msg": "ID 및 Password를 확인해주세요."
             })
         else:
+            if not session.get('logged_in') in session:
+                session['logged_in'] = True
+            
+            print("SESSION:", session.get('logged_in'))
+
             return jsonify({
                 "result": "success",
                 "msg": "로그인 성공"
             })
             
-@app.route('/notice', methods=['GET'])
+@app.route('/notice', methods=['GET', 'POST'])
 def notice():
-    return render_template('notice/notice_list.html')
+    if request.method == 'GET':
+        if session['logged_in']:
+            return render_template('notice/notice_list.html')
+        else:
+            return redirect(url_for('login'))
+    elif request.method == 'POST':
+        return render_template('notice/notice_write.html')
+    
+        
 
 @app.route('/main', methods=['GET'])
 def main():
